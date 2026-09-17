bdfb8d1925a8827bd3069f65cfce42f8cf918ae6
FabianT <fabianskytoma@gmail.com>
2026-09-17 21:22:40 +0300

    resolved messages bug

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import NotFound from "./pages/NotFound";
 import ProfilePage from "./pages/profile/ProfilePage";
 import FriendsPage from "./pages/friends/FriendsPage";
 import SettingsPage from "./pages/settings/SettingsPage";
-import MessagesPage from "./components/messages/MessagesPage";
+import MessagesPage from "./components/MessagesPage";
 
 function App() {
   return (
